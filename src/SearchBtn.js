6b20dcd86e2dab480c50a75cc1e017db8840787e
litaesther10 <estherlitalg@gmail.com>
2022-09-01 13:54:00 +0200

    Updated the SearchBtn to make it more intuitive

--- src/SearchBtn.js
+++ src/SearchBtn.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function SearchBtn() {
   return (
-    <div className="fixed text-xl top-20 left-10 lg:left-32 2xl:left-96 md:left-18 sm:left-">
+    <div className="fixed left-0 text-sm top-20 lg:left-16 2xl:left-60 md:left-4 sm:left-1 lg:text-xl">
       <a href="./#Search ">
-        <button className="px-2 py-1 transition border-2 rounded-full shadow shadow-sky-200 border-sky-200 hover:shadow-lg ">
-          🔍
+        <button className="px-2 py-1 text-gray-500 transition border-2 rounded-full shadow shadow-sky-200 border-sky-200 hover:shadow-lg">
+          🔍 Search
         </button>
       </a>
     </div>
